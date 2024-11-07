dd65af95b6e87c55ed0af84e574917c2e55577a8
vanessa.loja <vanessa.lloja@unl.edu.ec>
2024-11-07 01:42:30 -0500

    feat: styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
     host: true,
   },
+
+  integrations: [react(), tailwind({applyBaseStyles:false})],
 });
