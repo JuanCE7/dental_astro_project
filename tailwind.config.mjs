c27887f3043fe34eb62657a68c13c559d585af75
JuanCE7 <juancastiestrella@gmail.com>
2025-04-11 10:51:49 -0500

    feat: company info and treatments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#2781C5",
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2781C5",
+        secondary: '#1A1A1A'
+      },
+	  fontFamily: {
+        cormo: ['"cormorant-garamond"', 'serif'], // defines una familia "libre"
+      },
+    },
+  },
+  plugins: [],
+};
