98fe5d0889c807857c843300843839349dbd19ae
JuanCE7 <juancastiestrella@gmail.com>
2025-04-10 21:38:15 -0500

    feat: header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   server: {
     port: 3000,
     host: true,
   },
+  integrations: [tailwind()],
 });
